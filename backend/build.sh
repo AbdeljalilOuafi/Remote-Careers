57b63e5ac7b655cad0229b6ddac4595af47ffd10
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-14 20:47:00 +0100

    superuser2

--- backend/build.sh
+++ backend/build.sh
@@ -4,7 +4,11 @@ pip install -r backend/requirements.txt
 
 python backend/manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]
-then
-    python backend/manage.py createsuperuser --no-input
+if [[ $CREATE_SUPERUSER == "True" ]]; then
+    python backend/manage.py createsuperuser --no-input \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL" \
+        --first_name "Admin" \  # You can set a default first name
+        --last_name "User"      # You can set a default last name
+    echo "Superuser created."
 fi
