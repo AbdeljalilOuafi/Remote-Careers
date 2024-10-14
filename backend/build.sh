10bce22a89e4b042f34e66e2b7f89173a182c9e0
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-14 19:38:30 +0100

    fix_deploy

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,7 @@
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r backend/requirements.txt
 
-python manage.py collectstatic --no-input
+python backend/manage.py collectstatic --no-input
 
-python manage.py migrate
+python backend/manage.py migrate
