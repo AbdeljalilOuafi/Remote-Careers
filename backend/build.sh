575ba4a1447bbba00f14606402c2157d6420a332
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-14 20:33:53 +0100

    superuser_build

--- backend/build.sh
+++ backend/build.sh
@@ -3,3 +3,8 @@ set -o errexit
 pip install -r backend/requirements.txt
 
 python backend/manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]]
+then
+    python backend/manage.py createsuperuser --no-input
+fi
