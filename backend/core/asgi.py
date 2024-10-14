11e2b932a21ecbc425dbd4f9d8dbb9ffceda5a60
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-14 19:11:04 +0100

    deployment_ready

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+settings_module = 'core.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'core.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
 application = get_asgi_application()
--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+settings_module = 'core.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'core.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
 application = get_wsgi_application()
