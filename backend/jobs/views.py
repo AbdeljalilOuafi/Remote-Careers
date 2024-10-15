b94ad8a24e5a6585fce62e61cb4c7fa400accbcd
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-15 17:19:54 +0100

    refactor: Increase Performance

--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -3,15 +3,17 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 import requests
+from decouple import config
 
 class GetJobs(GenericAPIView):
     def get(self, request):
         # Define the new URL for searching jobs
-        url = "https://linkedin-data-api.p.rapidapi.com/search-jobs?keywords=Javascript&locationId=92000000&datePosted=anyTime&onsiteRemote=remote&sort=mostRelevant"
+        # url = "https://linkedin-data-api.p.rapidapi.com/search-jobs?keywords=Javascript&locationId=92000000&datePosted=anyTime&onsiteRemote=remote&sort=mostRelevant"
+        url = "https://linkedin-data-api.p.rapidapi.com/search-jobs?keywords=golang%20javascript%20python%20nodejs%20react&locationId=92000000&datePosted=anyTime&titleIds=9&onsiteRemote=remote&sort=mostRelevant"
 
         headers = {
             'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com',
-            'x-rapidapi-key': '6f63eb34a8mshc31062713a84ad8p1e9478jsnce509864bb73'
+            'x-rapidapi-key': config('RAPIDAPI_KEY')
         }
 
         # Make the GET request to the external API
