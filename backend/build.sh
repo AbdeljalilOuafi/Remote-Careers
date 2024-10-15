a7005701d665c5069221dbc05293e0caab5ed972
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-15 10:10:33 +0100

    disable_superuser_script

--- backend/build.sh
+++ backend/build.sh
@@ -4,10 +4,10 @@ pip install -r backend/requirements.txt
 
 python backend/manage.py migrate
 
-if [[ $CREATE_SUPERUSER == "True" ]]; then
-    python backend/manage.py createsuperuser --no-input \
-        --email "$DJANGO_SUPERUSER_EMAIL" \
-        --first_name "${DJANGO_SUPERUSER_FIRST_NAME}" \
-        --last_name "${DJANGO_SUPERUSER_LAST_NAME}"
-    echo "Superuser created."
-fi
+# if [[ $CREATE_SUPERUSER == "True" ]]; then
+#     python backend/manage.py createsuperuser --no-input \
+#         --email "$DJANGO_SUPERUSER_EMAIL" \
+#         --first_name "${DJANGO_SUPERUSER_FIRST_NAME}" \
+#         --last_name "${DJANGO_SUPERUSER_LAST_NAME}"
+#     echo "Superuser created."
+# fi
