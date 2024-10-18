1066fa8a628b59bc66b1eebd823095bbaf1dcdf3
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-18 12:35:43 +0100

    axios

--- frontend/src/axios/axios.jsx
+++ frontend/src/axios/axios.jsx
@@ -4,14 +4,13 @@ import axios from 'axios';
 // Create an instance of Axios with a custom configuration
 const axiosInstance = axios.create({
   // this
-    // baseURL: 'https://linkedin-data-api.p.rapidapi.com', // Set to the root URL
-    baseURL: 'https://api.remotecareers.tech/api', // Set to the root URL
+    baseURL: 'https://linkedin-data-api.p.rapidapi.com', // Set to the root URL
+    // baseURL: 'https://api.remotecareers.tech/api', // Set to the root URL
     timeout: 10000, // Optional: set a timeout for requests
     headers: {
         'Content-Type': 'application/json',
-        // this
-        // 'x-rapidapi-key': '26c47ff7bbmsh20045f047178745p1f4642jsna6fcb588c3e8', // Move API key here
-        // 'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com' // Move host here
+        'x-rapidapi-key': '26c47ff7bbmsh20045f047178745p1f4642jsna6fcb588c3e8', // Move API key here
+        'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com' // Move host here
     },
 });
 
